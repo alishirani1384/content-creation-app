079860b95ec5e0b32684eb4d33a5a78e374800ba
Ali Shirani <alishiranii1384@gmail.com>
2024-09-25 02:05:26 +0330

    changes applied

---
+++ src/components/sidebar/SideBar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react'
+
+function SideBar() {
+  return (
+      <div className='lg:w-1/4 lg:block w-full lg:static'>
+          
+    </div>
+  )
+}
+
+export default SideBar
